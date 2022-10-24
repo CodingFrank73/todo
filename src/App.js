f727704f775edc47689a489fee8f8049712880ad
codingfrank <codingfrank73@gmail.com>
2022-10-24 12:33:51 +0200

    add main structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 
 import './App.css';
 
+import { useState } from 'react';
+import NewTodoForm from './components/NewTodoForm';
+import TodoList from './components/TodoList';
+
+
+
 function App() {
+  const [todos, setTodos] = useState([]);
+
   return (
     <div className="App">
-
+      <NewTodoForm />
+      <TodoList />
     </div>
   );
 }
---
+++ src/components/NewTodoForm.jsx
@@ -0,0 +1,7 @@
+const NewTodoForm = () => {
+    return (
+        <div></div>
+    );
+}
+
+export default NewTodoForm;
